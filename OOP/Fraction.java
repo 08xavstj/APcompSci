dfbe1d6874c20899e117c44c2cda4c699d6ff09b
Xavier St-Jacques <08xavstj@gmail.com>
2020-01-27 13:36:32 -0800

    Add files via upload

---
+++ OOP/Fraction.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+class Fraction {
+    private static int a,b;
+    public Fraction() {
+        this.a = 0;
+        this.b = 1;
+    }
+    
+    public Fraction(int x,int y) {
+        this.a = a;
+        if (b == 0) {
+            System.out.println("broken");
+            b = 1;
+        } else {
+            this.b = b;
+        }
+    }
+    
+    public Fraction(String f) {
+        int str = f.indexOf("/");
+        String num = f.substring(0,str);
+        String den = f.substring(str+1);
+        this.a = Integer.parseInt(num);       
+        if (den.equals("0")) {
+            den = "1";
+        } else {
+            this.b = Integer.parseInt(den);
+        }
+    }
+    
+    public Fraction(Fraction f) {
+        this.a = f.a;
+        this.b = f.b;
+    }
+}
