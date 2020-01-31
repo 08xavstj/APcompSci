d628ef05170884009bcef5c35cad1295b03b2594
Xavier St-Jacques <08xavstj@gmail.com>
2020-01-31 14:05:08 -0800

    Add files via upload

--- OOP/Fraction.java
+++ OOP/Fraction.java
@@ -5,7 +5,7 @@ class Fraction {
         this.a = 0;
         this.b = 1;
     }
-    
+
     public Fraction(int x,int y) {
         this.a = a;
         if (b == 0) {
@@ -15,7 +15,7 @@ class Fraction {
             this.b = b;
         }
     }
-    
+
     public Fraction(String f) {
         int str = f.indexOf("/");
         String num = f.substring(0,str);
@@ -27,9 +27,39 @@ class Fraction {
             this.b = Integer.parseInt(den);
         }
     }
-    
+
     public Fraction(Fraction f) {
         this.a = f.a;
         this.b = f.b;
     }
+
+    public int getA() {
+        return this.a;
+    }
+
+    public int getB() {
+        return this.b;
+    }
+
+    public String toString() {
+        return a+"/"+b;
+    }
+
+    public double toDouble() {
+        return a/b;
+    }
+
+    public void reduce() {
+     
+    }
+    
+    public void setNum() {
+        
+    }
+    
+    public void setDen() {
+        
+    }
+    
+    
 }
