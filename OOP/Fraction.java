e53c00d1dcd83aee6a20f8c6f2c6c9bb3550f20f
Xavier St-Jacques <08xavstj@gmail.com>
2020-02-04 11:22:22 -0800

    Add files via upload

--- OOP/Fraction.java
+++ OOP/Fraction.java
@@ -50,16 +50,67 @@ class Fraction {
     }
 
     public void reduce() {
-     
+        int gcf = GCF(this.a, this.b);
+        this.a = this.a/gcf;
+        this.b = this.b/gcf;
+    }
+
+    public void setNum(int a) {
+        this.a = a;
+    }
+
+    public void setDen(int b) {
+        this.b = b;
     }
     
-    public void setNum() {
+    public static Fraction multiply(Fraction numer, Fraction denom) {
+        int num,den;
+        num = numer.a * denom.a;
+        den = numer.b * denom.b;
+        Fraction f = new Fraction(num,den);
+        f.reduce();
+        return f;        
+    }
+    
+    public static Fraction divide(Fraction numer, Fraction denom) {
+        int num,den;
+        num = numer.a * denom.b;
+        den = numer.b * denom.a;
+        Fraction f = new Fraction(num,den);
+        f.reduce();
+        return f;        
+    }
+    
+    public static Fraction add(Fraction numer, Fraction denom) {
+        int com = numer.b * denom.b;
+        numer.a = numer.a * denom.b;
+        denom.a = denom.a * numer.b;
         
+        Fraction f = new Fraction(numer.a+denom.b, com);
+        return f;
     }
     
-    public void setDen() {
+    public static Fraction subtract(Fraction numer, Fraction denom) {
+        int com = numer.b * denom.b;
+        numer.a = numer.a * denom.b;
+        denom.a = denom.a * numer.b;
         
+        Fraction f = new Fraction(numer.a-denom.b, com);
+        return f;
     }
     
-    
+    private static int GCF(int num, int den) {
+        num = Math.abs(num); 
+        den = Math.abs(den);
+
+        while (num != den) {
+            if (num>den) {
+                num = num - den;
+            }
+            if (num<den) {
+                den = den - num;
+            }
+        }
+        return num;
+    }
 }
---
+++ OOP/test.java
@@ -0,0 +1,7 @@
+class test{
+    public static void main(String args[]) {
+        Fraction f1 = new Fraction(3,4);
+        Fraction f2 = new Fraction(3,5);
+        System.out.println(Fraction.subtract(f1,f2));
+    }
+}
